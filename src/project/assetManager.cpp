d2c82a0a1713576b906d3b9b45af8cb58caee47f
retrozelda <efolckemer@retrozelda.com>
2026-05-09 23:56:57 -0400

    prefabs can be in any directory

--- src/project/assetManager.cpp
+++ src/project/assetManager.cpp
@@ -671,7 +671,7 @@ void Project::AssetManager::save()
       continue;
     }
 
-    entry->prefab->save();
+    entry->prefab->save(entry->path);
     dirtyPrefabs.erase(uuid);
     savedPrefabState.erase(uuid);
   }
